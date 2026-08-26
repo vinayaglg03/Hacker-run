938923f4aa7641b5f2eb550f3093bea98923ae9e
codedbyvinaya <vinayagalagali03@gmail.com>
2026-08-26 13:06:16 +0530

    Update printing-pattern-2.c

--- partA/printing-pattern-2.c
+++ partA/printing-pattern-2.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
 
+int main() 
+{
+
+    int n;
+    scanf("%d", &n);
+    
+    int size= 2*n-1;
+  	for(int i=0;i<size;i++){
+        for(int j=0;j<size;j++){
+            int top=i;
+            int bottom=size-1-i;
+            int left=j;
+            int right=size-1-j;
+            
+            int min=top;
+            
+            if(bottom<min)
+            min=bottom;
+            
+             if (left < min)
+                min = left;
+
+            if (right < min)
+                min = right;
+
+            printf("%d ", n - min);
+        }
+        printf("\n");
+    }
+    return 0;
+}
