b5c09d0cf8cdc96b3276d0c6bb12d17ea61ba598
codedbyvinaya <vinayagalagali03@gmail.com>
2026-08-26 12:50:44 +0530

    Update playing-with-characters.c

--- partA/playing-with-characters.c
+++ partA/playing-with-characters.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+// #include <string.h>
+// #include <math.h>
+// #include <stdlib.h>
+
+int main() 
+{
+    char ch;
+    char s[100], sen[100];
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    
+    scanf("%c", &ch);
+    printf("%c\n", ch);
+    
+    scanf("%s", s);
+    printf("%s\n", s);
+    
+    scanf(" %[^\n]%*c", sen);
+    printf("%s\n", sen);
+        
+    return 0;
+}
 
