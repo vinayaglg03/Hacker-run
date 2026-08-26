8d650a1fc4482a7fd6d387b9cdda0bc8785aa524
codedbyvinaya <vinayagalagali03@gmail.com>
2026-08-26 12:51:23 +0530

    Update sum-numbers-c.c

--- partA/sum-numbers-c.c
+++ partA/sum-numbers-c.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a,b;
+    float c,d;
+    
+    scanf("%d%d", &a,&b);
+    printf("%d\t%d\n", a+b, a-b );
+    
+    scanf("%f%f", &c,&d);
+    printf("%.1f\t%.1f", c+d, c-d );
+    
+    return 0;
+}
 
