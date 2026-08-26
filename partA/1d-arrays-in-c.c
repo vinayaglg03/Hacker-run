8c90b6fd1bfa87909fd1e2382ff0cc5d1a1d92f7
codedbyvinaya <vinayagalagali03@gmail.com>
2026-08-26 12:54:36 +0530

    Update 1d-arrays-in-c.c

--- partA/1d-arrays-in-c.c
+++ partA/1d-arrays-in-c.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int n, i,sum=0;
+    scanf("%d", &n);
+    int *arr= malloc(n*sizeof(int));
+    
+    for(i=0; i<n; i++){
+        scanf("%d", &arr[i]);
+    }
+    
+    for(i=0;i<n;i++){
+        sum+=arr[i];
+    }
+    printf("%d",sum); 
+    free(arr);  
+    return 0;
+}
 
