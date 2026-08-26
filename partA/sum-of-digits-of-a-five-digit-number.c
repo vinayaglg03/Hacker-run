d51277e1274077a4252807f138e22041a84ea09c
codedbyvinaya <vinayagalagali03@gmail.com>
2026-08-26 12:53:52 +0530

    Update sum-of-digits-of-a-five-digit-number.c

--- partA/sum-of-digits-of-a-five-digit-number.c
+++ partA/sum-of-digits-of-a-five-digit-number.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n, digit=0;
+    scanf("%d", &n);
+    while (n>0){
+        digit=digit+n%10;
+        n/=10;
+    }
+    printf("%d", digit);
+    
+    return 0;
+}
 
