86e87c44a5118b6e2ebb4692b77731d2c9e204c0
codedbyvinaya <vinayagalagali03@gmail.com>
2026-08-26 12:49:56 +0530

    Update hello-world-c.c

--- partA/hello-world-c.c
+++ partA/hello-world-c.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^\n]%*c", &s);
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    printf("Hello, World!\n");
+    scanf("%s",s);
+    printf("Welcome to C programming.");   
+    return 0;
+}
 
