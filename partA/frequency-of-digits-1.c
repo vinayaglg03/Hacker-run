2360f7a113ebe66708dbc6c58a4a99a7379c26c5
codedbyvinaya <vinayagalagali03@gmail.com>
2026-08-26 13:32:28 +0530

    Implement digit frequency counter in C
    Count the frequency of each digit in the input string and print the results.

--- partA/frequency-of-digits-1.c
+++ partA/frequency-of-digits-1.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    char str[1000];
+    int count[10]={0};
+    scanf("%s",str);
+    
+    for(int i=0;str[i]!='\0';i++){
+        if(str[i]>='0' && str[i]<='9'){
+            int digit=str[i]-'0';
+            count[digit]++;
+        }
+    }
+    for(int i=0; i<10; i++){
+        printf("%d ",count[i]);
+    }
+    
+    return 0;
+}
 
